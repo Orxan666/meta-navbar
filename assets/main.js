b43141602ab78c4dbd2dc9d1fc250300ad15d6b6
Orxan666 <orxanallahyarov03@gmail.com>
2025-01-16 22:45:01 +0400

    initial commit

---
+++ assets/main.js
@@ -0,0 +1,7 @@
+let globus=document.querySelector(".res-navbar ul .globus")
+let globus_menu=document.querySelector(".res-navbar ul .globus .drop-menu-tel")
+globus.addEventListener("click",function(){
+    globus_menu.classList.toggle("active")
+
+
+})
